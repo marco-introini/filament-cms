4969894bf70c9c84fc9e459357f08f2a80dc4beb
Marco "Mint" Introini <mint.dev@pm.me>
2024-07-18 11:10:29 +0200

    Update templates and homepage content
    The templates have been updated with a new set of classes for layout and styling purposes. Additionally, the homepage content has been modified with a new introductory message. Instead of a dummy text, a brief description of the project is now showed. Also, the posts index page now renders inside the main application template, improving the visual consistency across the app.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,5 @@
 <x-templates.template>
-    Prova
+    <div class="mt-5">
+        This is an example project for creating a CMS like application in Filament
+    </div>
 </x-templates.template>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,12 @@
+<x-templates.template>
 
-<ul>
-@foreach($posts as $post)
-    <li>{{$post->title}}</li>
-@endforeach
-</ul>
+    <h1 class="text-3xl">
+        Posts
+    </h1>
+
+    <ul>
+        @foreach($posts as $post)
+            <li>{{$post->title}}</li>
+        @endforeach
+    </ul>
+</x-templates.template>
